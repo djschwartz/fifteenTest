3adb873a9c581bbb4603ae529813eb2d05339cb1
Dan Schwartz <dschwartz@appareo.com>
2023-09-28 11:53:20 -0700

    Initial Commit

---
+++ fifteenTest/fifteenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifteenTestApp.swift
+//  fifteenTest
+//
+//  Created by Dan Schwartz on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct fifteenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
