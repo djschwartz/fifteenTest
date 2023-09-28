1741256cf7a568cd245bd9ee3a442f6e9bf412d1
Dan Schwartz <dschwartz@appareo.com>
2023-09-28 14:00:57 -0700

    Add single assert true.

--- fifteenTestTests/fifteenTestTests.swift
+++ fifteenTestTests/fifteenTestTests.swift
@@ -18,6 +18,10 @@ final class fifteenTestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testTrue() {
+        assert(true)
+    }
+
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
